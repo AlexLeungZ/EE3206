f59a6f2fb01d9590414fa1b967ac9ef5bd039af5
AlexLeungZ <alexleungchunwai@gmail.com>
2021-09-15 00:33:02 +0800

    20210914

--- src/App.java
+++ src/App.java
@@ -1,6 +1,15 @@
+import java.util.Random;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println(message(args));
+
+        var curTime = new java.util.Date();
+        var randInt = new Random(curTime.getTime());
+
+        System.out.println(curTime);
+        for (int i = 0; i < 10; i++)
+            System.out.print(randInt.nextInt(1000) + " ");
     }
 
     public static String message(String... values) {
--- src/ex2/TestCircle2.java
+++ src/ex2/TestCircle2.java
@@ -9,8 +9,8 @@ public class TestCircle2 {
 
         // Display c1 BEFORE c2 is created
         System.out.println("Before creating c2");
-        System.out.println("c1 is : radius (" + c1.radius +
-                ") and number of Circle objects (" + c1.numberOfObjects + ")");
+        System.out.println(
+                "c1 is : radius (" + c1.radius + ") and number of Circle objects (" + c1.numberOfObjects + ")");
 
         // Create c2
         Circle2 c2 = new Circle2(5);
@@ -20,9 +20,9 @@ public class TestCircle2 {
 
         // Display c1 and c2 AFTER c2 was created
         System.out.println("\nAfter creating c2 and modifying c1's radius to 9");
-        System.out.println("c1 is : radius (" + c1.radius +
-                ") and number of Circle objects (" + c1.numberOfObjects + ")");
-        System.out.println("c2 is : radius (" + c2.radius +
-                ") and number of Circle objects (" + c2.numberOfObjects + ")");
+        System.out.println(
+                "c1 is : radius (" + c1.radius + ") and number of Circle objects (" + c1.numberOfObjects + ")");
+        System.out.println(
+                "c2 is : radius (" + c2.radius + ") and number of Circle objects (" + c2.numberOfObjects + ")");
     }
 }
